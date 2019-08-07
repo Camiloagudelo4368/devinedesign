f7687a44ebc02c9b148cfdb3ad04487d2eb534d5
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-08-07 00:42:58 -0400

    build folder created

---
+++ client/build/precache-manifest.df7d36cba803236fb74aa5e0ad337858.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cad2e282d2ede6571302b433f7c0cbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65768db3652028f64bb",
+    "url": "/static/css/main.3f4bf2f6.chunk.css"
+  },
+  {
+    "revision": "84de75d0c9179d65c6dd",
+    "url": "/static/js/2.0597359c.chunk.js"
+  },
+  {
+    "revision": "d65768db3652028f64bb",
+    "url": "/static/js/main.26fce176.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb26c2e06331e9d83fdcb472333f312d",
+    "url": "/static/media/slideimage_1.cb26c2e0.jpg"
+  },
+  {
+    "revision": "ff92650e44a9d9236888d9a31c035530",
+    "url": "/static/media/slideimage_10.ff92650e.jpg"
+  },
+  {
+    "revision": "cdd0d967db917ee160e991530a19dbd9",
+    "url": "/static/media/slideimage_2.cdd0d967.jpeg"
+  },
+  {
+    "revision": "0afa159563402993f8bfc8e5d894b44b",
+    "url": "/static/media/slideimage_3.0afa1595.jpeg"
+  },
+  {
+    "revision": "f10579ef7284912995d4fcea27caf153",
+    "url": "/static/media/slideimage_4.f10579ef.jpeg"
+  },
+  {
+    "revision": "bb33d40c46d454461eae4e661cc24415",
+    "url": "/static/media/slideimage_5.bb33d40c.jpeg"
+  },
+  {
+    "revision": "99886067e58362aaf85af3d89ea5f4c1",
+    "url": "/static/media/slideimage_6.99886067.jpg"
+  },
+  {
+    "revision": "4c947702124b3c591fa7a11c9bd7f06c",
+    "url": "/static/media/slideimage_7.4c947702.jpeg"
+  },
+  {
+    "revision": "9a6a9f2823854be9e0456673a3064e24",
+    "url": "/static/media/slideimage_8.9a6a9f28.jpeg"
+  },
+  {
+    "revision": "5fb63bcac88a43ba464cef8e7c23fff6",
+    "url": "/static/media/slideimage_9.5fb63bca.jpeg"
+  }
+]);
